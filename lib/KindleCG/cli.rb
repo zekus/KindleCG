fb28a6483b1e50952d13321ae5b53249e0f13b27
Antonio Lorusso <antonio.lorusso@gmail.com>
2013-07-09 15:08:19 +0200

    you can specify the kindle mountpoint from the command line now

--- lib/KindleCG/cli.rb
+++ lib/KindleCG/cli.rb
@@ -3,8 +3,14 @@ require 'KindleCG'
 
 module KindleCG
   class CLI < Thor
+    method_option :mountpoint, type: :string, aliases: "-m",
+      desc: "specify a different mountpoint for the kindle device"
     desc 'generate', 'generate and save the collections'
     def generate
+      if options[:mountpoint]
+        KindleCG.os_mountpoint = options[:mountpoint]
+      end
+
       check
 
       kindle = Generator.new
@@ -12,13 +18,17 @@ module KindleCG
       kindle.save
     end
 
+    method_option :mountpoint, type: :string, aliases: "-m",
+      desc: "specify a different mountpoint for the kindle device"
     desc 'check', 'check if a kindle is attached to the computer'
     def check
-      unless File.directory?(KindleCG.os_mountpoint)
-        raise Thor::Error, "Could not find any kindle attached to #{KindleCG.os_mountpoint}"
+      mountpoint = options[:mountpoint] || KindleCG.os_mountpoint
+
+      unless File.directory?(mountpoint)
+        raise Thor::Error, "Could not find any kindle attached to #{mountpoint}"
         exit 1
       else
-        say "Your kindle is attached to #{KindleCG.os_mountpoint}. Very good! :)", :green
+        say "Your kindle is attached to #{mountpoint}. Very good! :)", :green
       end
     end
   end
